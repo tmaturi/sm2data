937bb7e719d6b42f626c341bdf12cf388412f6de
tcm <tcm@tcms-MacBook-Air.local>
2021-12-13 21:18:14 +0000

    add the animals data

---
+++ R/animals.R
@@ -0,0 +1,15 @@
+#' Survival Times of Animals
+#'
+#' The  data frame records the survival times of 48 animals given different poisons and treatments.
+#' 
+#' @usage
+#' data(animals)
+#' 
+#' @format This data frame has 48 observations on 3 variables:
+#' \describe{
+#'   \item{Poison}{factor levels: I, II, III}
+#'   \item{Treatment}{factor levels: A, B, C, D }
+#'   \item{Survival}{numeric: animal's survival time measured in 10 hour units} 
+#' }
+#' @source Handbook of Small Datasets; originally in Box and Cox (1964), JRSS A, volume 143, pages 383-430.
+"animals"
